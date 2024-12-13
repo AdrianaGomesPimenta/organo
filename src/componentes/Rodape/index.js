213ef31b38a97c599b5b538e2428c258a16fa459
AdrianaGomesPimenta <adriana.agpimenta@gmail.com>
2024-12-13 13:59:22 -0300

    desafio organo

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,11 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (
+        <footer className="rodape">
+            <img src="/imagens/Rodapé.png" alt="O rodape principal da página do Organo"/>
+        </footer>
+    )   
+}
+
+export default Rodape
