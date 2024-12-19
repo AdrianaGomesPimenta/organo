453ffe136c2ba7f9f63877d604b3c1a585d9d7b2
AdrianaGomesPimenta <adriana.agpimenta@gmail.com>
2024-12-19 14:45:01 -0300

    Criar novo  time

--- src/App.js
+++ src/App.js
@@ -3,96 +3,320 @@ import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
 import Rodape from './componentes/Rodape';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
-  const times = [
+  const [times, setTimes] = useState([
     {
+      id: uuidv4 (),
       nome: 'Advogado Trabalhista',
-      corPrimaria: '#57C278',
-      corSecundaria: '#D9F7E9'
+      cor: '#57C278'
     },
     {
+      id: uuidv4 (),
       nome: 'Advogado Criminal',
-      corPrimaria: '#82CFFA',
-      corSecundaria: '#E8F8FF'
+      cor: '#82CFFA'
     },
     {
+      id: uuidv4 (),
       nome: 'Advogado Civil',
-      corPrimaria: '#A6D157',
-      corSecundaria: '#F0F8E2'
+      cor: '#A6D157'
     },
     {
+      id: uuidv4 (),
       nome: 'Advogado Empresarial',
-      corPrimaria: '#E06B69',
-      corSecundaria: '#FDE7E8'
+      cor: '#E06B69'
     },
     {
+      id: uuidv4 (),
       nome: 'Advogado Tributário',
-      corPrimaria: '#DB6EBF',
-      corSecundaria: '#FAE9F5'
+      cor: '#DB6EBF'
     },
     {
+      id: uuidv4 (),
       nome: 'Advogado Previdenciário',
-      corPrimaria: '#FFBA05',
-      corSecundaria: '#FFF5D9'
+      cor: '#FFBA05'
     },
     {
+      id: uuidv4 (),
       nome: 'Advogado de Família e Sucessões',
-      corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF'
+      cor: '#FF8A29'
     },
     {
+      id: uuidv4 (),
       nome: 'Advogado Imobiliário',
-      corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF'
+      cor: '#57C278'
     },
     {
+      id: uuidv4 (),
       nome: 'Advogado Imobiliário',
-      corPrimaria: '#57C278',
-      corSecundaria: '#D9F7E9'
+      cor: '#82CFFA'
     },
     {
+      id: uuidv4 (),
       nome: 'Advogado Ambiental',
-      corPrimaria: '#82CFFA',
-      corSecundaria: '#E8F8FF'
+      cor: '#A6D157'
     },
     {
+      id: uuidv4 (),
       nome: 'Advogado Administrativo',
-      corPrimaria: '#A6D157',
-      corSecundaria: '#F0F8E2'
+      cor: '#E06B69'
     },
     {
+      id: uuidv4 (),
       nome: 'Advogado Consumerista',
-      corPrimaria: '#E06B69',
-      corSecundaria: '#FDE7E8'
+      cor: '#DB6EBF'
     },
     {
+      id: uuidv4 (),
       nome: 'Advogado Constitucional',
-      corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF'
+      cor: '#FFBA05'
     }
+  ])
+
+  const inicial = [
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'JULIANA',
+      cargo: 'Advogado Trabalhista',
+      imagem: 'https://framerusercontent.com/images/HvaoJ9qTHCtnW1SEelLo2ssRuE.webp',
+      time: times[0].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'DANIEL',
+      cargo: 'Advogado Trabalhista',
+      imagem: 'https://media.licdn.com/dms/image/v2/D4D22AQHAU0--I3YqbA/feedshare-shrink_800/feedshare-shrink_800/0/1732901356352?e=2147483647&v=beta&t=3VdeZRtWL9OX7sFV-oZKgO5bwkv4OJGUCg_MWN9GlAE',
+      time: times[0].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'GUILHERME',
+      cargo: 'Advogado Trabalhista',
+      imagem: '	https://framerusercontent.com/images/23Y3Ud5oGJihytX4eWG5zckWHeQ.png',
+      time: times[0].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'PAULO',
+      cargo: 'Advogado Trabalhista',
+      imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQPtxUOEcsUbZnW2Tn1FSE_J_sGJJnoNH63Ae1mKx4z7pzHM535OdmhqRKZLjeCC-xC5A&usqp=CAU',
+      time: times[0].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'JULIANA',
+      cargo: 'Advogado Criminal',
+      imagem: 'https://framerusercontent.com/images/HvaoJ9qTHCtnW1SEelLo2ssRuE.webp',
+      time: times[1].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'DANIEL',
+      cargo: 'Advogado Criminal',
+      imagem: 'https://media.licdn.com/dms/image/v2/D4D22AQHAU0--I3YqbA/feedshare-shrink_800/feedshare-shrink_800/0/1732901356352?e=2147483647&v=beta&t=3VdeZRtWL9OX7sFV-oZKgO5bwkv4OJGUCg_MWN9GlAE',
+      time: times[1].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'GUILHERME',
+      cargo: 'Advogado Criminal',
+      imagem: '	https://framerusercontent.com/images/23Y3Ud5oGJihytX4eWG5zckWHeQ.png',
+      time: times[1].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'PAULO',
+      cargo: 'Advogado Criminal',
+      imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQPtxUOEcsUbZnW2Tn1FSE_J_sGJJnoNH63Ae1mKx4z7pzHM535OdmhqRKZLjeCC-xC5A&usqp=CAU',
+      time: times[1].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'JULIANA',
+      cargo: 'Advogado Civil',
+      imagem: 'https://framerusercontent.com/images/HvaoJ9qTHCtnW1SEelLo2ssRuE.webp',
+      time: times[2].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'DANIEL',
+      cargo: 'Advogado Civil',
+      imagem: 'https://media.licdn.com/dms/image/v2/D4D22AQHAU0--I3YqbA/feedshare-shrink_800/feedshare-shrink_800/0/1732901356352?e=2147483647&v=beta&t=3VdeZRtWL9OX7sFV-oZKgO5bwkv4OJGUCg_MWN9GlAE',
+      time: times[2].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'GUILHERME',
+      cargo: 'Advogado Civil',
+      imagem: '	https://framerusercontent.com/images/23Y3Ud5oGJihytX4eWG5zckWHeQ.png',
+      time: times[2].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'PAULO',
+      cargo: 'Advogado Civil',
+      imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQPtxUOEcsUbZnW2Tn1FSE_J_sGJJnoNH63Ae1mKx4z7pzHM535OdmhqRKZLjeCC-xC5A&usqp=CAU',
+      time: times[2].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'JULIANA',
+      cargo: 'Advogado Empresarial',
+      imagem: 'https://framerusercontent.com/images/HvaoJ9qTHCtnW1SEelLo2ssRuE.webp',
+      time: times[3].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'DANIEL',
+      cargo: 'Advogado Empresarial',
+      imagem: 'https://media.licdn.com/dms/image/v2/D4D22AQHAU0--I3YqbA/feedshare-shrink_800/feedshare-shrink_800/0/1732901356352?e=2147483647&v=beta&t=3VdeZRtWL9OX7sFV-oZKgO5bwkv4OJGUCg_MWN9GlAE',
+      time: times[3].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'GUILHERME',
+      cargo: 'Advogado Empresarial',
+      imagem: '	https://framerusercontent.com/images/23Y3Ud5oGJihytX4eWG5zckWHeQ.png',
+      time: times[3].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'PAULO',
+      cargo: 'Advogado Empresarial',
+      imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQPtxUOEcsUbZnW2Tn1FSE_J_sGJJnoNH63Ae1mKx4z7pzHM535OdmhqRKZLjeCC-xC5A&usqp=CAU',
+      time: times[3].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'JULIANA',
+      cargo: 'Advogado Tributário',
+      imagem: 'https://framerusercontent.com/images/HvaoJ9qTHCtnW1SEelLo2ssRuE.webp',
+      time: times[4].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'DANIEL',
+      cargo: 'Advogado Tributário',
+      imagem: 'https://media.licdn.com/dms/image/v2/D4D22AQHAU0--I3YqbA/feedshare-shrink_800/feedshare-shrink_800/0/1732901356352?e=2147483647&v=beta&t=3VdeZRtWL9OX7sFV-oZKgO5bwkv4OJGUCg_MWN9GlAE',
+      time: times[4].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'GUILHERME',
+      cargo: 'Advogado Tributário',
+      imagem: '	https://framerusercontent.com/images/23Y3Ud5oGJihytX4eWG5zckWHeQ.png',
+      time: times[4].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'PAULO',
+      cargo: 'Advogado Tributário',
+      imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQPtxUOEcsUbZnW2Tn1FSE_J_sGJJnoNH63Ae1mKx4z7pzHM535OdmhqRKZLjeCC-xC5A&usqp=CAU',
+      time: times[4].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'JULIANA',
+      cargo: 'Advogado Previdenciário',
+      imagem: 'https://framerusercontent.com/images/HvaoJ9qTHCtnW1SEelLo2ssRuE.webp',
+      time: times[5].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'DANIEL',
+      cargo: 'Advogado Previdenciário',
+      imagem: 'https://media.licdn.com/dms/image/v2/D4D22AQHAU0--I3YqbA/feedshare-shrink_800/feedshare-shrink_800/0/1732901356352?e=2147483647&v=beta&t=3VdeZRtWL9OX7sFV-oZKgO5bwkv4OJGUCg_MWN9GlAE',
+      time: times[5].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'GUILHERME',
+      cargo: 'Advogado Previdenciário',
+      imagem: '	https://framerusercontent.com/images/23Y3Ud5oGJihytX4eWG5zckWHeQ.png',
+      time: times[5].nome
+    },
+    {
+      id: uuidv4 (),
+      favorito:false,
+      nome: 'PAULO',
+      cargo: 'Advogado Previdenciário',
+      imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQPtxUOEcsUbZnW2Tn1FSE_J_sGJJnoNH63Ae1mKx4z7pzHM535OdmhqRKZLjeCC-xC5A&usqp=CAU',
+      time: times[5].nome
+    },
   ]
 
-  const [colaboradores, setColaboradores] = useState([])
+  const [colaboradores, setColaboradores] = useState(inicial)
 
-  const aoNovoColaboradorAdicionado = (colaborador) => {
-    debugger
-    setColaboradores([...colaboradores, colaborador])
+  function deletarColaborador(id) {
+    setColaboradores(colaboradores.filter(colaborador => colaborador.id !== id));
   }
 
-  return (
-    <div className="App">
-      <Banner />
-      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+  function mudarCorDoTime (cor, id) {
+    setTimes(times.map(time => {
+      if(time.id === id) {
+        time.cor = cor;
+      }
+      return time;
+    }));
+  }
 
-      {times.map(time => <Time 
-        key={time.nome} 
-        nome={time.nome} 
-        corPrimaria={time.corPrimaria} 
-        corSecundaria={time.corSecundaria} 
-        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
-      />)}   
+  function cadastrarTime(novoTime) {
+    setTimes([ ...times, { ...novoTime, id: uuidv4() }])
+  }
+
+  function resolverFavorito(id) {
+    setColaboradores(colaboradores.map(colaborador => {
+      if(colaborador.id === id) colaborador.favorito = !colaborador.favorito;
+      return colaborador
+    }))
+  }
+  
+  return (
+    <div>
+      <Banner />
+      <Formulario 
+      cadastrarTime={cadastrarTime}
+        times={times.map(time => time.nome)} 
+        aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} 
+      />
+      <section className="times">
+        <h1>Minha organização</h1>
+        {times.map((time, indice) => 
+          <Time 
+            aoFavoritar={resolverFavorito}
+            mudarCor={mudarCorDoTime }
+            key={indice} 
+            time={time} 
+            colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} 
+            aoDeletar={deletarColaborador}
+          />
+        )}
+      </section>
       <Rodape />
     </div>
   );
